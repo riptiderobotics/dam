be8c8e1c51a4299740423753fddc4bb12e449d7f
alokthakrar <alokthakrar1@gmail.com>
2024-02-15 20:17:51 -0800

    adding rr tuning utilities

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoToLimits.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoToLimits.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,24 +9,34 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 
+@Config
 @TeleOp(name="servoTester", group="Linear Opmode")
 // decorator so the robot knows what mode to operate in
 public class ServoToLimits extends LinearOpMode {
     // Declare teleOp members.
     private ElapsedTime runtime = new ElapsedTime();
-    public static int onOff = 1;
+    public static double power = 0;
     Servo outtakeFlip1;
     Servo outakeFlip2;
     Servo outakeRelease;
     Servo droneLauncher;
+
+    DcMotor pullup0;
+    DcMotor pullup1;
+
     @Override
     // related to inheritance: runOpMode is a necessary function as you NEED to override the runOpMode in the superclass
     public void runOpMode() {
-        //System.out on the phones = telemetry
+        /*
         outtakeFlip1 = hardwareMap.servo.get("OuttakeFlip1");
         outakeFlip2 = hardwareMap.servo.get("OutakeFlip2");
         outakeRelease = hardwareMap.servo.get("OutakeRelease");
         droneLauncher = hardwareMap.servo.get("droneLauncher");
+        /*
+         */
+        pullup0 = hardwareMap.dcMotor.get("pullup0");
+        pullup1 = hardwareMap.dcMotor.get("pullup1");
+
 
         // int encoderValue = 0;
         // Reverse the right side motors
@@ -44,6 +55,7 @@ public class ServoToLimits extends LinearOpMode {
             return;
 
         while (opModeIsActive()) {
+            /*
             if(gamepad1.a){
                 outtakeFlip1.setPosition(0);
             }
@@ -64,6 +76,12 @@ public class ServoToLimits extends LinearOpMode {
                 droneLauncher.setPosition(0);
             if(gamepad1.dpad_right)
                 droneLauncher.setPosition(1);
+        }
+        */
+
+
+            pullup0.setPower(-power);
+            pullup1.setPower(power);
         }
     }
 }
